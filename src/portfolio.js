c5ab3546d8132e5991cf9ff3c9dad77d47b0daf2
Jaidev1807 <77259043+Jaidev1807@users.noreply.github.com>
2024-05-30 14:44:26 -0400

    first

---
+++ src/portfolio.js
@@ -0,0 +1,613 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: false, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello.",
+  title2: "Jaidev",
+  logo_name: "Jaidev.p()",
+  nickname: "hrishi1999 / picleric",
+  full_name: "Jaidev Patel",
+  subTitle:
+    "Full Stack Developer, Software Developer Enthusiast. Always learning.",
+  resumeLink:
+    "https://drive.google.com/file/d/1nAIlSb9knMk7Xvx0SS7zJZxSGZ3vzjje/view?usp=drive_link",
+  mail: "mailto:jaidevpatel1807@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/Jaidev1807",
+  linkedin: "https://www.linkedin.com/in/jaidev-patel-5894a318a/",
+  gmail: "jaidevpatel1807@gmail.com",
+  gitlab: " ",
+  facebook: " ",
+  twitter: "https://x.com/JaidevPatel10",
+  instagram: "https://www.instagram.com/jaidev_1807/",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building responsive website's front end using ReactJS and Angular",
+        "⚡ Developing mobile applications using Flutter and native Android",
+        "⚡ Backend development using NodeJS, ExpressJS and Flask",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "PHP",
+          fontAwesomeClassname: "simple-icons:php",
+          style: {
+            color: "#7377AD",
+          },
+        },
+        {
+          skillName: "Java",
+          fontAwesomeClassname: "simple-icons:java",
+          style: {
+            color: "#f89820",
+          },
+        },
+        {
+          skillName: "Kotlin",
+          fontAwesomeClassname: "simple-icons:kotlin",
+          style: {
+            color: "#5c79df",
+          },
+        },
+        {
+          skillName: "Dart",
+          fontAwesomeClassname: "simple-icons:dart",
+          style: {
+            color: "#29B0EE",
+          },
+        },
+        {
+          skillName: "Go",
+          fontAwesomeClassname: "simple-icons:go",
+          style: {
+            color: "#00A7D0",
+          },
+        },
+        {
+          skillName: "Visual Basic",
+          fontAwesomeClassname: "simple-icons:dot-net",
+          style: {
+            color: "#029FCE",
+          },
+        },
+        {
+          skillName: "C#",
+          fontAwesomeClassname: "simple-icons:csharp",
+          style: {
+            backgroundColor: "transparent",
+            color: "#3776AB",
+          },
+        },
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C++",
+          fontAwesomeClassname: "simple-icons:cplusplus",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NextJS",
+          fontAwesomeClassname: "simple-icons:nextdotjs",
+          style: {
+            color: "#FFFFFF",
+            backgroundColor: "#1D1D1D",
+            borderRadius: "50%",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+        {
+          skillName: "GraphQL",
+          fontAwesomeClassname: "simple-icons:graphql",
+          style: {
+            color: "#DE33A6",
+          },
+        },
+        {
+          skillName: "Android App Development",
+          fontAwesomeClassname: "simple-icons:android",
+          style: {
+            color: "#3DDC84",
+          },
+        },
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+
+        {
+          skillName: "Wordpress",
+          fontAwesomeClassname: "simple-icons:wordpress",
+          style: {
+            color: "#207297",
+          },
+        },
+
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+      ],
+    },
+
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Certified AWS Developer Associate and Cloud Practioner",
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Experience hosting and managing web apps on GCP and AWS",
+        "⚡ Deploying deep learning models on cloud to use on mobile devices",
+      ],
+      softwareSkills: [
+        {
+          skillName: "GCP",
+          fontAwesomeClassname: "simple-icons:googlecloud",
+          style: {
+            color: "#4285F4",
+          },
+        },
+        {
+          skillName: "AWS",
+          fontAwesomeClassname: "simple-icons:amazonaws",
+          style: {
+            color: "#FF9900",
+          },
+        },
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Heroku",
+          fontAwesomeClassname: "simple-icons:heroku",
+          style: {
+            color: "#6863A6",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+        {
+          skillName: "Docker",
+          fontAwesomeClassname: "simple-icons:docker",
+          style: {
+            color: "#1488C6",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Durham college",
+      subtitle: "Cybersecurity",
+      logo_path: "durham.png",
+      alt_name: "DAL",
+      duration: "2024 - 2024",
+      descriptions: [
+        "⚡ Mastered Advanced Cybersecurity Techniques, Network Security & Threat Management.",
+        "⚡ Explored Topics in Ethical Hacking, Penetration Testing, and Incident Response.",
+        "⚡ Developed projects on courses like Cryptography and Secure Application Development.",
+      ],
+      website_link: "https://www.durhamcollege.ca/",
+    },
+    {
+      title: "Humber college",
+      subtitle: "Web Design and Development",
+      logo_path: "humber.png",
+      alt_name: "Humber",
+      duration: "2023 - 2023",
+      descriptions: [
+        "⚡ Learned Advanced Web Design Principles, UX/UI Design & Front-End Development.",
+        "⚡ Studied Advanced Topics in Responsive Design, Web Accessibility, and SEO.",
+        "⚡ Created projects on courses like Full-Stack Development and Progressive Web Apps.",
+      ],
+      website_link: "https://humber.ca/",
+    },
+    {
+      title: "LDRP Institute of Technology and Research",
+      subtitle: "B.Tech. in Computer Engineering",
+      logo_path: "LDRP.png",
+      alt_name: "LDRP",
+      duration: "2018 - 2022",
+      descriptions: [
+        "⚡ Studied core subjects like Data Structures, DBMS, Networking, Security, etc.",
+        "⚡ Completed various online courses for ML/DL, Web Development, Mobile App Development, etc.",
+        "⚡ Implemented several projects based on what I've learnt under my Computer Engineering course. ",
+      ],
+      website_link: "https://www.ldrp.ac.in/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "AWS Developer Associate",
+      subtitle: "Amazon Web Services",
+      logo_path: "aws.png",
+      certificate_link:
+        "https://www.credly.com/badges/b12fbece-07f3-47f5-9dda-cd56f49e250d/public_url",
+      alt_name: "AWS",
+      color_code: "#ffc475",
+    },
+
+    {
+      title: "System Design Foundation",
+      subtitle: "Linkedin",
+      logo_path: "Linkedin.png",
+      certificate_link: " ",
+      alt_name: "Google",
+      color_code: "#1e70c1",
+    },
+    {
+      title: "Google Summer of Code Mentor",
+      subtitle: " 2020",
+      logo_path: "google_logo.png",
+      certificate_link: " ",
+      alt_name: "Google",
+      color_code: "#ffbfar",
+    },
+    {
+      title: "DSA Developer Certificate",
+      subtitle: "Google",
+      logo_path: "google_logo.png",
+      certificate_link: " ",
+      alt_name: "Google",
+      color_code: "#ffbfae",
+    },
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "I've successfully completed an internship and a Co-op, gaining valuable hands-on experience. Most of my projects have been self-driven, showcasing my ability to take initiative. I'm currently eager to secure a Co-op position starting Fall 2024. Additionally, I enjoy organizing workshops to disseminate my knowledge and foster learning within the community.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work",
+      experiences: [
+        {
+          title: "Full Stack Developer",
+          company: "Enrage Tech",
+          company_url: "https://enragetech.com/",
+          logo_path: "enrage.png",
+          duration: "Sept 2023 - Dec 2023",
+          location: "Vaughn , Ontario , Canada",
+          description:
+            "Developed and maintained web applications using Vue JS ,Laravel, PHP, WordPress, and similar technologies.Ensured cross-device compatibility and responsive design for seamless user experiences.Collaborated with multidisciplinary teams to plan, implement, and test new features.Upheld coding standards and best practices throughout the development process.Actively engaged in continuous learning to enhance application functionality and performance.Stayed updated with emerging technologies to drive improvements in efficiency and speed",
+          color: "#2962FF",
+        },
+        {
+          title: "Web Developer",
+          company: "Unada Labs",
+          company_url: "https://unada.io/",
+          logo_path: "unada.png",
+          duration: "May 2022 - Dec 2022",
+          location: "Ahmedabad, Gujarat",
+          description:
+            "Developed full stack web applications using the MERN (MongoDB, Express.js, React.js, Node.js) stack. Designed and implemented RESTful APIs to handle server-side logic and data manipulation. Utilized MongoDB for database management, including schema design and querying. Implemented responsive and dynamic user interfaces using React.js for frontend development. Leveraged Node.js and Express.js to build scalable and efficient server-side applications. Collaborated with cross-functional teams to deliver high-quality software solutions, meeting project deadlines and requirements. Conducted code reviews, identified bugs, and implemented optimizations to enhance application performance and maintainability.",
+          color: "#f10000",
+        },
+      ],
+    },
+    {
+      title: "Internships",
+      experiences: [
+        {
+          title: "Web Developer",
+          company: "Acmegrade",
+          company_url: "https://acmegrade.com/",
+          logo_path: "acmegrade.png",
+          duration: "Feb 2022 - Apr 2022",
+          location: "",
+          description: "Developed a website using HTML , CSS , PHP and jQuery.",
+          color: "#56A4D3",
+        },
+      ],
+    },
+    {
+      title: "Volunteerships",
+      experiences: [
+        {
+          title: "Google Summer of Code Mentor",
+          company: "Sugar Labs / GSoC",
+          company_url: "https://sugarlabs.org/",
+          logo_path: "google_logo.png",
+          duration: "Summer of 20",
+          location: " ",
+          description:
+            "Mentorship responsibilities were to help students plan the project, review issues and pull requests, ensure smooth progress and help them out when they are stuck.",
+          color: "#4285F4",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects incorporate a diverse range of cutting-edge technology tools. I specialize in crafting dynamic website projects, where I've honed my skills the most. Here are some examples of my work, although not all are currently available on GitHub.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "Jaidev.jpg",
+    description:
+      "Feel free to reach out to me at any of the contact points listed below. I'll do my best to respond promptly. ",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "While I may not publish blogs on a frequent basis, I make it a point to share noteworthy accomplishments by crafting detailed and engaging articles when possible.",
+    link: "https://medium.com/@jaidevpatel1807",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      name: "CineScope",
+      url: "https://cinescope.netlify.app/",
+      description: "An IMDB-like application.",
+      languages: [
+        {
+          name: "React.js",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "TypeScript",
+          iconifyClass: "devicon-typescript",
+        },
+        {
+          name: "Node.js",
+          iconifyClass: "devicon-nodejs",
+        },
+        {
+          name: "Express.js",
+          iconifyClass: "devicon-express",
+        },
+        {
+          name: "MongoDB",
+          iconifyClass: "logos-mongodb",
+        },
+      ],
+    },
+    {
+      name: "Tony,sLiquor",
+      url: "https://www.mytonysliquor.com/",
+      description: "An E-commerce application made as a Freelances for client.",
+      languages: [
+        {
+          name: "React.js",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Python Django",
+          iconifyClass: "logos-python",
+        },
+        {
+          name: "MongoDB",
+          iconifyClass: "logos-mongodb",
+        },
+      ],
+    },
+    {
+      name: "RecipesAPI",
+      url: "https://rapidapi.com/Hrishi1999/api/recipesapi2",
+      description: "Get recipes for hundreds and thousands of dishes",
+      languages: [
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+      ],
+    },
+    {
+      name: "Cab Security System",
+      url: " ",
+      description:
+        "Driver drowsiness detection, profanity detection, violence detection, SMS alerts, cab driver tracking.",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+        {
+          name: "Tensorflow",
+          iconifyClass: "logos-tensorflow",
+        },
+        {
+          name: "OpenCV",
+          iconifyClass: "logos-opencv",
+        },
+      ],
+    },
+
+    {
+      name: "Project Pickle",
+      url: " ",
+      description:
+        "An app similar to Uber Eats but targeted for Gruhudhyogs (homemade pickles and products). Order management, live order tracking, rating and reviews system, etc.",
+      languages: [
+        {
+          name: "Java",
+          iconifyClass: "logos-java",
+        },
+        {
+          name: "Firebase",
+          iconifyClass: "logos-firebase",
+        },
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+      ],
+    },
+    {
+      name: "Project Health Card",
+      url: "",
+      description:
+        "An aadhard-card like utility card which keeps track of all your health records suchs as: Medicines, Lab Reports, Recent Diseases, Allergies, etc.",
+      languages: [
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-java",
+        },
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+        {
+          name: "Firebase",
+          iconifyClass: "logos-firebase",
+        },
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "MongoDB",
+          iconifyClass: "logos-mongodb",
+        },
+      ],
+    },
+    {
+      name: "Food Classifier",
+      url: " ",
+      description:
+        "A food classifier app using tflite which classfier over 50 Indian Dishes.",
+      languages: [
+        {
+          name: "Flutter",
+          iconifyClass: "logos-flutter",
+        },
+        {
+          name: "Dart",
+          iconifyClass: "logos-dart",
+        },
+        {
+          name: "Tensorflow",
+          iconifyClass: "logos-tensorflow",
+        },
+        {
+          name: "Jupyter Notebook",
+          iconifyClass: "logos-jupyter",
+        },
+      ],
+    },
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
